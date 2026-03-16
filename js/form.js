4d89c4f12b733d9f2a1bfedc23034e7f84cbf87f
Binarywiseweb <fronted001.binarywise+002GH@gmail.com>
2026-03-17 04:56:55 +0530

    update contact form

--- js/form.js
+++ js/form.js
@@ -1,20 +1,21 @@
-(function($) {
+(function ($) {
 'use strict';
 
 var contactForm = {
+
     form: '#contact-form',
     invalidCls: 'is-invalid',
-    validation: '[name="username"],[name="email"],[name="phone"],[name="message"]',
+    validation: '[name="username"],[name="email"],[name="phone"],[name="address"],[name="form_work_type"],[name="message"]',
     emailField: '[name="email"]',
     messages: $('<p class="form-messages mb-0 mt-3"></p>'),
 
     emailjsConfig: {
         serviceId: 'service_ypxzbva',
-        templateId: 'template_lrb1mmk',
+        templateId: 'template_538mfhj',
         publicKey: 'KeeDIZDTSrheB0Ymz'
     },
 
-    init: function() {
+    init: function () {
 
         if ($(this.form + ' .form-messages').length === 0) {
             $(this.form).append(this.messages);
@@ -25,16 +26,17 @@ var contactForm = {
         this.bindEvents();
     },
 
-    bindEvents: function() {
+    bindEvents: function () {
+
         var self = this;
 
-        $(this.form).on('submit', function(e) {
+        $(this.form).on('submit', function (e) {
             e.preventDefault();
             self.handleSubmit();
         });
     },
 
-    handleSubmit: function() {
+    handleSubmit: function () {
 
         if (this.validateForm()) {
 
@@ -44,6 +46,7 @@ var contactForm = {
                 username: $(this.form + ' [name="username"]').val(),
                 email: $(this.form + ' [name="email"]').val(),
                 phone: $(this.form + ' [name="phone"]').val(),
+                address1: $(this.form + ' [name="address1"]').val(),
                 work_type: $(this.form + ' [name="form_work_type"]').val(),
                 message: $(this.form + ' [name="message"]').val()
             };
@@ -52,21 +55,21 @@ var contactForm = {
         }
     },
 
-    validateForm: function() {
+    validateForm: function () {
 
         var isValid = true;
         var self = this;
 
         $(this.form + ' input, ' + this.form + ' textarea, ' + this.form + ' select')
-        .removeClass(this.invalidCls);
+            .removeClass(this.invalidCls);
 
         var fields = this.validation.split(',');
 
-        fields.forEach(function(field) {
+        fields.forEach(function (field) {
 
             var $field = $(self.form + ' ' + field.trim());
 
-            if (!$field.val().trim()) {
+            if (!$field.val() || !$field.val().trim()) {
                 $field.addClass(self.invalidCls);
                 isValid = false;
             }
@@ -88,7 +91,7 @@ var contactForm = {
         return isValid;
     },
 
-    showLoading: function() {
+    showLoading: function () {
 
         this.messages.removeClass('error success');
         this.messages.addClass('loading');
@@ -97,33 +100,31 @@ var contactForm = {
         $(this.form + ' button[type="submit"]').prop('disabled', true);
     },
 
-    showSuccess: function() {
+    showSuccess: function () {
 
         this.messages.removeClass('error loading');
         this.messages.addClass('success');
-
         this.messages.html('Message sent successfully!');
 
         $(this.form + ' button[type="submit"]').prop('disabled', false);
     },
 
-    showError: function(message) {
+    showError: function (message) {
 
         this.messages.removeClass('success loading');
         this.messages.addClass('error');
-
         this.messages.html(message);
 
         $(this.form + ' button[type="submit"]').prop('disabled', false);
     },
 
-    clearForm: function() {
+    clearForm: function () {
 
         $(this.form)[0].reset();
         $(this.form + ' input, textarea, select').removeClass(this.invalidCls);
     },
 
-    sendWithEmailJS: function(formData) {
+    sendWithEmailJS: function (formData) {
 
         var self = this;
 
@@ -132,26 +133,26 @@ var contactForm = {
             this.emailjsConfig.templateId,
             {
                 to_name: "Young Gunns Roofing",
-                to_email: "admin1@gmail.com,admin2@gmail.com",
+                to_email: "younggunnsroofing@gmail.com",
 
                 username: formData.username,
                 email: formData.email,
                 phone: formData.phone,
+                address: formData.address,
                 work_type: formData.work_type,
                 message: formData.message,
 
                 reply_to: formData.email
             },
             this.emailjsConfig.publicKey
-
-        ).then(function(response) {
+        ).then(function (response) {
 
             console.log("Email Sent", response);
 
             self.showSuccess();
             self.clearForm();
 
-        }, function(error) {
+        }, function (error) {
 
             console.log("Email Error", error);
 
@@ -162,7 +163,7 @@ var contactForm = {
 
 };
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     if (contactForm.emailjsConfig.publicKey) {
         emailjs.init(contactForm.emailjsConfig.publicKey);
